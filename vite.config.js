cba66cd2caf11568694cd7b38d49019f381350e8
Harshith Yallampalli <harshithyallampalli@Harshiths-MacBook-Pro.local>
2025-05-22 17:07:21 -0400

    fix: remove custom base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default {
-  base: '/investor_tool', // replace with actual GitHub repo name
+  base: '/', // replace with actual GitHub repo name
 };
